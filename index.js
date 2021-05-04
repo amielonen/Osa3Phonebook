8fc5adc55a4d02ed74483c366360f54d9edd96a0
amielonen <81672251+amielonen@users.noreply.github.com>
2021-05-04 10:47:16 +0300

    changes to app

--- index.js
+++ index.js
@@ -106,7 +106,8 @@ const generateID = () => {
     return maxID+1
 }
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001;
+
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+    console.log(`server running on port ${PORT}`);
+});
